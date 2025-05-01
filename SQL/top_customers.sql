f1ba3dc5c3693d32a3c6f4f3cfa3ba14e40f9706
anuani-78 <anu.anunitha9@gmail.com>
2025-05-01 14:07:02 -0400

    Update top_customers.sql

--- SQL/top_customers.sql
+++ SQL/top_customers.sql
@@ -1,1 +1,13 @@
+-- customers table
+CREATE TABLE customers (
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(100)
+);
+
+-- orders table
+CREATE TABLE orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE
+);
 
